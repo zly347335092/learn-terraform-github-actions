4d1d410e66701b5fcc998cc74cba3e0920e4576e
zly347335092 <347335092@qq.com>
2022-04-01 22:45:18 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,30 +24,22 @@ data "alicloud_images" "ubuntu" {
   name_regex  = "^ubuntu_18.*64"
 }
 
-module "ecs_cluster" {
-  source  = "alibaba/ecs-instance/alicloud"
+  
+resource "alicloud_instance" "instance" {
+  # cn-beijing
+  availability_zone = "cn-chengdu-a"
+  security_groups = ["sg-2vc4r8v7gd43watyfjr0"]
 
-  number_of_instances = 1
-
-  name                        = "my-ecs-cluster"
-  use_num_suffix              = true
-  image_id                    = data.alicloud_images.ubuntu.ids.0
-  instance_type               = "ecs.s6-c1m2.large"
-  vswitch_id                  = "vsw-2vckzmhrxxoce4orouajw"
-  security_group_ids          = ["sg-2vc4r8v7gd43watyfjr0"]
-  associate_public_ip_address = true
-  internet_max_bandwidth_out  = 10
-
-  password = "Asd123456"
-
+  # series III
+  instance_type        = "ecs.s6-c1m2.large"
   system_disk_category = "cloud_ssd"
-  system_disk_size     = 50
-
-  tags = {
-    Created      = "Terraform"
-    Environment = "dev14"
-  }
+  image_id             = data.alicloud_images.ubuntu.ids.0
+  instance_name        = "test_foo"
+  vswitch_id = "vsw-2vckzmhrxxoce4orouajw"
+  internet_max_bandwidth_out =10
+  password = "Asd123456"
 }
+  
 output "web-address" {
-  value = "${module.ecs_cluster.*.this_instance_id}"
+  value = "${alicloud_instance.instance.id}"
 }
